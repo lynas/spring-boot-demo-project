05d669c9c905bec67e09b8df58185cb38b1ffdb0
Sazzad Islam <sazzad.islam@reev.com>
2023-04-04 09:52:01 +0200

    test

--- src/main/kotlin/com/lynas/demo/DemoApplication.kt
+++ src/main/kotlin/com/lynas/demo/DemoApplication.kt
@@ -2,10 +2,18 @@ package com.lynas.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<DemoApplication>(*args)
+}
+
+@RequestMapping("/")
+class DemoController{
+    @GetMapping
+    fun get() = "hello"
 }
